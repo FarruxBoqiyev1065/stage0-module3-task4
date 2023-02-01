ef7c67b42c3e155738878b20b7949cd7666edc54
macservis <fboqiyev1065@gmail.com>
2023-02-01 15:58:08 +0500

    module3-task4 uploaded

--- src/main/java/lang/print/gaps/task4/SettingTypes.java
+++ src/main/java/lang/print/gaps/task4/SettingTypes.java
@@ -2,6 +2,11 @@ package lang.print.gaps.task4;
 
 public class SettingTypes {
     public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+
+        c = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b)/2);
 //         first = 10_000_000_000L;
 //         second = 'a';
 //         third = 5 > 9;
